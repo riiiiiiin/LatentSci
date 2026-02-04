3c084c9abc8d8f3406fbeb00b81c338570736cd2
std::bad_wheel <109677966+riiiiiiin@users.noreply.github.com>
2026-02-04 16:50:11 +0800

    Fixed hf download include format

--- scripts/data/prepare_test_data.sh
+++ scripts/data/prepare_test_data.sh
@@ -2,8 +2,8 @@
 
 # Download test datasets
 echo "Downloading test datasets..."
-huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChemCoTBench" --local-dir ./test-data/
-huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChemLLMBench" --local-dir ./test-data/
-huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "Mol-Instructions" --local-dir ./test-data/
-huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChEBI" --local-dir ./test-data/
+huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChemCoTBench/**" --local-dir ./test-data/
+huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChemLLMBench/**" --local-dir ./test-data/
+huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "Mol-Instructions/**" --local-dir ./test-data/
+huggingface-cli download --repo-type dataset anonymousssss22321/latentchem --include "ChEBI/**" --local-dir ./test-data/
 
